701edf993eb379d46a68a736ece16b8e1a8c688d
Christophe Desplanches <kcdesplanches@gmail.com>
2026-02-18 12:43:37 -0500

    feat: implement industrial Level 3 gRPC architecture with shared library

---
+++ shared/grpc-lib/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = path.join(__dirname, './protos/service.proto');
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+module.exports = {
+    service: protoDescriptor.service,
+    protoPath: PROTO_PATH
+};
